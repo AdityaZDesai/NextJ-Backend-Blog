a2f5af95940fd5ce460151befa4e174324ffc1b2
Aditya Desai <adityadesai753@gmail.com>
2024-07-14 17:48:07 +1000

    edits

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
-const port = 5000;
+const port = 3000;
 app.use(express.json()); // Middleware to parse JSON bodies
 console.log(process.env.MONGODB_URL)
 mongoose.connect(process.env.MONGODB_URL, {
